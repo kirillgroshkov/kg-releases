ff8947b18cce7afb007e6d9b9bdcd5844c19669e
kirillgroshkov <kirill.groshkov@naturalcycles.com>
2018-04-29 01:35:25 +0200

    fix build

--- src/cmp/RootComponent.vue
+++ src/cmp/RootComponent.vue
@@ -8,9 +8,8 @@
 import Vue from 'vue'
 import Component from 'vue-class-component'
 
-@Component()
-export default class RootComponent extends Vue {
-}
+@Component
+export default class RootComponent extends Vue {}
 </script>
 
 <style lang="scss" scoped>
