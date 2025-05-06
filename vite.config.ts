5eb6080b41ef38de53398f430317b745cbf465a4
kirillgroshkov <kirill.groshkov@naturalcycles.com>
2025-05-07 00:20:51 +0200

    chore: add rollup-plugin-replace

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,31 @@
-import { fileURLToPath } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vuetify from 'vite-plugin-vuetify'
 import { visualizer } from 'rollup-plugin-visualizer'
+import replace from '@rollup/plugin-replace'
 
 export default defineConfig({
   plugins: [
     vue(),
     vuetify(),
+    replace({
+      preventAssignment: true,
+      __SENTRY_DEBUG__: false,
+      __SENTRY_TRACING__: false,
+      __RRWEB_EXCLUDE_IFRAME__: true,
+      __RRWEB_EXCLUDE_SHADOW_DOM__: true,
+      __SENTRY_EXCLUDE_REPLAY_WORKER__: true,
+    }),
     visualizer({
       template: 'flamegraph',
       filename: 'dist/stats.html',
       gzipSize: true,
     }),
   ],
+
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@': `${import.meta.dirname}/src`,
     },
   },
 })
