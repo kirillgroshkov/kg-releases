2616760e08f6c0c25879999ce175ae8cba90c446
kirillgroshkov <kirill.groshkov@naturalcycles.com>
2025-05-07 09:28:14 +0200

    chore: use sourcemap

--- vite.config.ts
+++ vite.config.ts
@@ -2,27 +2,28 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vuetify from 'vite-plugin-vuetify'
 import { visualizer } from 'rollup-plugin-visualizer'
-import replace from '@rollup/plugin-replace'
 
 export default defineConfig({
+  define: {
+    __SENTRY_DEBUG__: false,
+    __SENTRY_TRACING__: false,
+    __RRWEB_EXCLUDE_IFRAME__: true,
+    __RRWEB_EXCLUDE_SHADOW_DOM__: true,
+    __SENTRY_EXCLUDE_REPLAY_WORKER__: true,
+  },
   plugins: [
     vue(),
     vuetify(),
-    replace({
-      preventAssignment: true,
-      __SENTRY_DEBUG__: false,
-      __SENTRY_TRACING__: false,
-      __RRWEB_EXCLUDE_IFRAME__: true,
-      __RRWEB_EXCLUDE_SHADOW_DOM__: true,
-      __SENTRY_EXCLUDE_REPLAY_WORKER__: true,
-    }),
     visualizer({
       template: 'flamegraph',
       filename: 'dist/stats.html',
       gzipSize: true,
+      sourcemap: true,
     }),
   ],
-
+  build: {
+    sourcemap: true,
+  },
   resolve: {
     alias: {
       '@': `${import.meta.dirname}/src`,
